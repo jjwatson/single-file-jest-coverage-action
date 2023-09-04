ae7db98c5a16d0be6a4f1c9fe6b9b8f7e5465080
watsoj <jowatson@cambridge.org>
2023-09-04 14:34:30 +0100

    Action to combine a report

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const HTML_STRING = (cssContents, htmlBodyContents, jsContents) => `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Combined Report</title>
+        <style>
+            .coverage-summary .sorter {
+                background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAbCAYAAACwRpUzAAAAUUlEQVR4AWNwL/CBYyAwRuajSMAwTAxFAl0BWAIXBunEifFKEjYWmwRBrxAMBJyYMq/8x4WhKjAlYMZiKMDllf+Uupb+kgS9QjAQcAYfwYAHAE7jpLivkXjUAAAAAElFTkSuQmCC") !important;
+            }
+            ${cssContents.join('\n')}
+        </style>
+    </head>
+    <body>
+        ${htmlBodyContents}
+        <script>
+            ${jsContents.join('\n')}
+        </script>
+    </body>
+    </html>
+`;
+
+const doTheThings = async () => {
+    try {
+        const reportsDir = core.getInput('reports-directory');
+
+        const files = await fs.promises.readdir(reportsDir);
+
+        const cssContents = await Promise.all(files
+            .filter(file => file.endsWith('.css'))
+            .map(filename => fs.promises.readFile(path.join(reportsDir, filename), { encoding: 'utf-8' })));
+
+        const jsContents = await Promise.all(files
+            .filter(file => file.endsWith('.js'))
+            .map(filename => fs.promises.readFile(path.join(reportsDir, filename), { encoding: 'utf-8' })));
+
+        const htmlContents = await fs.promises.readFile(path.join(reportsDir, 'index.html'), { encoding: 'utf-8' });
+        const bodyStart = htmlContents.indexOf('<body>') + '<body>'.length;
+        const bodyEnd = htmlContents.lastIndexOf('</body>');
+        const htmlBodyContents = htmlContents.substring(bodyStart, bodyEnd);
+
+        const htmlString = HTML_STRING(cssContents, htmlBodyContents, jsContents);
+
+        await fs.promises.writeFile('combined-report.html', htmlString, { encoding: 'utf-8' });
+
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+};
+doTheThings();
+
